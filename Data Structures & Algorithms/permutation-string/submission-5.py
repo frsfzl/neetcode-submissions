fe2608c946f296d8d9924c9beb65f5501838e26d
Faris Faizal <46803654+frsfzl@users.noreply.github.com>
2026-07-25 11:07:11 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count = {}
+        for char in s1:
+            count[char] = count.get(char, 0) + 1
+        
+        l = 0
+        for r in range(len(s2)):
+            if s2[r] in count.keys():
+                count[s2[r]] -= 1
+            
+            if max(count.values()) == 0:
+                return True
+            
+            if r - l + 1 == len(s1):
+                if s2[l] in count:
+                    count[s2[l]] += 1
+                l += 1
+            
+        return False
